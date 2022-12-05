2a75714ca143b7cfbd8f5c10c6f6a884b693331a
anatolyev <anatolyev@yandexlyceum.ru>
2022-12-05 17:13:17 +0300

    создали Получение координат для клеток поля

--- main.py
+++ main.py
@@ -26,10 +26,21 @@ class Board:
                     y * self.cell_size + self.top,
                     self.cell_size, self.cell_size
                 ), 1)
-     
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click(cell)
+
+    def get_cell(self, mouse_pos):
+        x, y = (mouse_pos[0] - self.left) // self.cell_size, \
+               (mouse_pos[1] - self.top) // self.cell_size
+        if self.width >= x + 1 > 0 and self.height >= y + 1 > 0:
+            return x, y
+
+
+    def on_click(self, cell):
+        print("Была выбрана ячейка:", cell)
     
-
-
 
 def main():
     pygame.init()
@@ -42,6 +53,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                board.get_click(event.pos)
+
+
         screen.fill((0, 0, 0))
         board.render(screen)
         pygame.display.flip()
