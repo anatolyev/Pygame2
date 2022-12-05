a75f4b4e261b1a38b0199d1bdf479b54d05cb806
Алексей Анатольев <73399971+anatolyev@users.noreply.github.com>
2022-12-05 16:52:16 +0300

    Update main.py
    добавили метод set_view для параметров вывода клеток

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ class Board:
         self.left = 10
         self.top = 10
         self.cell_size = 30
-
+    
+    
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+    
 
     def render(self, screen):
         # создание поля
@@ -20,6 +26,8 @@ class Board:
                     y * self.cell_size + self.top,
                     self.cell_size, self.cell_size
                 ), 1)
+     
+    
 
 
 
